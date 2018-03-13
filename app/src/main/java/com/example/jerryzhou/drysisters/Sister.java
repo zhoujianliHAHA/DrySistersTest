c78f1343d6db0818b7d397e0d4871be8e827f183
admin <972804112@qq.com>
2018-03-13 11:01:40 +0800

    妹子实体类

---
+++ app/src/main/java/com/example/jerryzhou/drysisters/Sister.java
@@ -0,0 +1,108 @@
+package com.example.jerryzhou.drysisters;
+
+/**
+ * @author admin
+ *
+ * id : id
+ * createAt ： 时间
+ * desc ：
+ */
+public class Sister {
+    private String id;
+    private String createAt;
+    private String desc;
+    private String publishedAt;
+    private String source;
+    private String type;
+    private String url;
+    private boolean used;
+    private String who;
+
+    public Sister(){
+
+    }
+
+    public Sister(String id, String createAt, String desc, String publishedAt, String source, String type, String url, boolean used, String who) {
+        this.id = id;
+        this.createAt = createAt;
+        this.desc = desc;
+        this.publishedAt = publishedAt;
+        this.source = source;
+        this.type = type;
+        this.url = url;
+        this.used = used;
+        this.who = who;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(String createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(String publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+
+    public void setUsed(boolean used) {
+        this.used = used;
+    }
+
+    public String getWho() {
+        return who;
+    }
+
+    public void setWho(String who) {
+        this.who = who;
+    }
+}
